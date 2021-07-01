f95a859e02a217cf5304815033779fbbf7011806
HBA2 <HBA2@192.168.1.23>
2021-07-01 20:42:09 +0300

    SecondJavaStudy

---
+++ 2021Yaz/src/variables01/Variables001.java
@@ -0,0 +1,11 @@
+package variables01;
+
+public class Variables001 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hi!!!");
+		System.out.println("Heyyy!!");
+	}
+	
+}
---
+++ 2021Yaz/src/variables01/Variables01.java
@@ -0,0 +1,21 @@
+package variables01;
+
+import java.util.Scanner;
+
+public class Variables01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.println("Adýnýzý Giriniz:");
+      
+      String Isim = scan.nextLine();
+      
+      System.out.println("Adýnýz:" + Isim);
+      
+      scan.close();
+      
+	}
+
+}
